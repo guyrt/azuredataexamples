9802cae896d5d3941187c80e420b37cdb95e3515
Tommy Guy <richardtguy84@gmail.com>
2019-04-03 10:39:07 -0700

    Updated account keys

--- jumpbikeazurefunctions/DownloadNewData/write_to_blob.py
+++ jumpbikeazurefunctions/DownloadNewData/write_to_blob.py
@@ -1,13 +1,15 @@
 from azure.storage.blob import BlockBlobService
 from json import dumps
-from io import StringIO
+from io import BytesIO
+
+import os
 
 
 path = "sea/year={year}/month={month}/day={day}/hour={hour}/{filename}"
 container_name ='rawjumpbikedata'
 
 def write_blob(timestamp, bikedata):
-    block_blob_service = BlockBlobService(account_name='accountname', account_key='accountkey')
+    block_blob_service = BlockBlobService(account_name='jumpbikedata', account_key=os.environ['OutputBlobStorageKey'])
     resolved_path = path.format(
         year=timestamp.strftime('%Y'),
         month=timestamp.strftime('%m'),
@@ -18,4 +20,4 @@ def write_blob(timestamp, bikedata):
 
     data = '\n'.join([dumps(b) for b in bikedata])
 
-    block_blob_service.create_blob_from_stream(container_name, resolved_path, StringIO(data))
+    block_blob_service.create_blob_from_stream(container_name, resolved_path, BytesIO(data.encode('ascii')))
